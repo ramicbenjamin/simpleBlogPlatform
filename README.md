f9903c407341ddf5af5ffc7fb08f35d95ceb76cc
Benjamin Ramic <bramic2@etf.unsa.ba>
2016-11-14 23:03:46 +0100

    Zadaća2 commit.

--- README.md
+++ README.md
@@ -8,3 +8,62 @@ Projekat na predmetu Web Tehnologije, Elektrotehnički fakultet Sarajevo
 
 Jednostavna blog platforma za razmjenu tekstova, fotografija, linkova između korisnika bloga.
 
+# Zadaća_1 last commit.
+
+Urađeno je:
+
+  - Napravljene skice (Photoshop) za sve podstranice na stranici, oboje, i za standardne veličine ekrana kao i za mobilne uređaje.
+- Sve napravljene stranice su responzivne i imaju grid-view.
+- Pomoću media query-a je regulisan izgled za mobilne uređaje, meni se skalira za manje uređaje kako se veličina ekrana smanjuje, na početnoj stranici se izbacuju manje bitni članci i ostaju samo bitni. Sve ostale podstranice i komponente se skaliraju po procentima na manje ekrane.
+- Implementirane su sljedeće HTML forme:
+    *  LogIn (Dolazi se do njega klikom na LOGIN u meniju na početnoj stranici)
+    * noviPost (Dolazi se do njega klikom na NOVIPOST u meniju na početnoj stranici)
+    * postavke (Dolazi se do njih klikom na POSTAVKE u meniju na početnoj stranici)
+    * registracija (Dolazi se do nje klikom na NEMAM AKAUNT na LOGIN stranici u podnožju)
+- Meni webstranice je implementiran i nalazi se na svim podstranicama, (osim na podstranicama LogIn i Registracija, jer nema smisla da se korisniku nudi meni sve dok nije registrovan).
+- Izgled stranice je konzistentan, nema glitcheva, elementi na stranici su poravnati.
+
+
+U testiranju nije primjećen nijedan bug.
+
+Lista fajlova:
+- css folder -> stil.css (Svi stilovi za web stranicu)
+- img folder (prazan, u njega treba da se dodaju slike koje stranica bude koristila)
+- simpleBlogPlatformPSDS (Nalaze se sve skice za stranicu, kao i PSD fajlovi istih)
+- clanakKompletan.html (Prikazuje cijeli članak nakon što se otvori sa početne stranice)
+- index.html (Početna stranica na kojoj su izlistani svi članci)
+- logIn.html (Stranica za login)
+- notifikacije.html (Prikazuje notifikacije logovanom korisniku)
+- noviPost.html (Otvara polja za unos novog posta, što podrazumijeva, naslov, tekst i sliku)
+- postavke.html (Nudi mogućnost za izmjenom ličnih podataka korisnika)
+- profil.html (Prikazuje profil logovanog korisnika)
+- registracija.html (Nudi mogućnost za unos podataka za registraciju korisnika)
+ 
+# Zadaća_2 last commit.
+
+Urađeno je:
+- Sva polja formi imaju validaciju koja je implementirana pomoću JavaScript-a (ispunjeni svi zahtjevi traženi u zadaći)
+- Koristeći JavaScript implementirane sljedeće dvije funckionalnosti
+    - Dropdown meni, koji se otvara kada se klikne na upitnik u izborniku
+    - Galerija slika sa opcijom da se na klik slika raširi preko cijelog ekrana, a na escape da se vrati pogled nazad na galeriju. Galerija se može pronaći kada se klikne sa početne stranice na "Naslov" bilo kojeg posta, gdje se otvori podstranica na kojoj se nalazi kompletan članak, a ispod njega galerija koja ispunjava sve zadane zahtjeve.
+- Koristeći Ajax napravljeno da se podstranice učitavaju bez reload-a cijele stranice, već da se samo sadržaj podstranice mijenja.
+
+U testiranju nije primjećen nijedan bug.
+
+Lista fajlova:
+
+- css folder -> stil.css (Svi stilovi za web stranicu)
+- img folder (nalaze se slike poztrebne za stranicu)
+- simpleBlogPlatformPSDS (Nalaze se sve skice za stranicu, kao i PSD fajlovi istih)
+- clanakKompletan.html (Prikazuje cijeli članak nakon što se otvori sa početne stranice)
+- views folder
+    - logInView.html (Stranica za login)
+    - notifikacijeView.html (Prikazuje notifikacije logovanom korisniku)
+    - noviPostView.html (Otvara polja za unos novog posta, što podrazumijeva, naslov, tekst i sliku)
+    - postavkeView.html (Nudi mogućnost za izmjenom ličnih podataka korisnika)
+    - profilView.html (Prikazuje profil logovanog korisnika)
+    - registracijaView.html (Nudi mogućnost za unos podataka za registraciju korisnika)
+    - clanakKompletanView.html (prikazije kompletan članak sa galerijom slika)
+    - pocetnaView.html (stranica na kojoj su izlistani svi članci)
+- js folder -> skripta.js (skripta u kojoj su opisane sve JS funkcionalnosti ove web stranice)
+- index.html (Početna stranica, u koju se loadaj svi view-si)
--- js/skripta.js
+++ js/skripta.js
@@ -1,27 +1,272 @@
-var idPokazanog;
-var prikazanaFotka = "";
-var prikazi = function (imeVar) {
-    idPokazanog = imeVar;
-    if (document.getElementById(idPokazanog+"_full").className == "shown" && prikazanaFotka != "") {
-        document.getElementById(idPokazanog+"_full").className = "hidden";
-        window.scrollTo(0, document.body.scrollHeight);
+/*Validacije*/
+/*loginValidacija*/
+function validirajLogIn() {
+    var uname = document.getElementById("unameLogin").value;
+    var pass = document.getElementById("passLogin").value;
+    if (!usernameIsValid(uname) || uname.length == 0) {
+        document.getElementById("greskaUname").className = "prikazGreske fontRoboto";
     }
-    else if (document.getElementById(idPokazanog+"_full").className == "hidden" && prikazanaFotka == "") {
-        document.getElementById(idPokazanog+"_full").className = "shown";
-        document.body.scrollTop = document.documentElement.scrollTop = 0;
-        prikazanaFotka = imeVar;
+    else {
+        document.getElementById("greskaUname").className = "hidden";
+    }
+    if (pass.length < 8 || pass.length == 0) {
+        document.getElementById("greskaPass").className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById("greskaPass").className = "hidden";
     }
 }
-var sakrij = function(id)
-{
-    document.getElementById(id).className = "hidden";
-    prikazanaFotka = "";
+/*ValidacijaRegistracije*/
+function validirajRegistraciju() {
+    var ime = document.getElementById("imeReg").value;
+    var prezime = document.getElementById("prezimeReg").value;
+    var username = document.getElementById("unameReg").value;
+    var pass1 = document.getElementById("passReg").value;
+    var pass2 = document.getElementById("passPReg").value;
+    var mail = document.getElementById("mailReg").value;
+    var datRodj = document.getElementById("rodjendanReg").value;
+    console.log(datRodj);
+    if (!usernameIsValid(ime) || ime.length == 0) {
+        document.getElementById("greskaImeReg").className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById("greskaImeReg").className = "hidden";
+    }
+    if (!usernameIsValid(prezime) || prezime.length == 0) {
+        document.getElementById("greskaPrezimeReg").className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById("greskaPrezimeReg").className = "hidden";
+    }
+    if (!usernameIsValid(username) || username.length == 0) {
+        document.getElementById("greskaUnameReg").className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById("greskaUnameReg").className = "hidden";
+    }
+    if (!emailValidation(mail) || mail.length == 0) {
+        document.getElementById("greskaMailReg").className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById("greskaMailReg").className = "hidden";
+    }
+    if (pass1.length == 0 || pass1.length < 8) {
+        document.getElementById("greskaPassReg").className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById("greskaPassReg").className = "hidden";
+    }
+    if (pass1.length == 0 || pass1 != pass2) {
+        document.getElementById("greskaPassPReg").className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById("greskaPassPReg").className = "hidden";
+    }
+    if (datRodj.length == 0) {
+        document.getElementById("greskaRodjReg").className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById("greskaRodjReg").className = "hidden";
+    }
+}
+var brojNeispravnih = 0; //da znam kad skloniti zabranu sa buttona
+function validirajImePrezime(validacija, greska) {
+    var ime = document.getElementById(validacija).value;
+    if (!usernameIsValid(ime) || ime.length == 0) {
+        document.getElementById(greska).className = "prikazGreske fontRoboto";
+        document.getElementById("regButton").disabled = true;
+        if (document.getElementById(greska).className != "prikazGreske fontRoboto") {
+            brojNeispravnih++;
+        }
+    }
+    else {
+        document.getElementById(greska).className = "hidden";
+        if (document.getElementById(greska).className != "hidden") {
+            brojNeispravnih--;
+        }
+        if (brojNeispravnih == 0) document.getElementById("regButton").disabled = false;
+    }
+}
+/*ValidacijeLogina*/
+function validirajUnameLogina() {
+    var uname = document.getElementById("unameLogin").value;
+    if (!usernameIsValid(uname) || uname.length == 0) {
+        document.getElementById("greskaUname").className = "prikazGreske fontRoboto";
+        document.getElementById("loginButton").disabled = true;
+    }
+    else {
+        document.getElementById("greskaUname").className = "hidden";
+        document.getElementById("loginButton").disabled = false;
+    }
+}
+
+function validirajJedanPass(passCont) {
+    var pass = document.getElementById(passCont).value;
+    if (pass.length < 8 || pass.length == 0) {
+        document.getElementById("greskaPass").className = "prikazGreske fontRoboto";
+        document.getElementById("loginButton").disabled = true;
+    }
+    else {
+        document.getElementById("greskaPass").className = "hidden";
+        document.getElementById("loginButton").disabled = false;
+    }
+}
+
+function validirajPassove() {
+    var pass1 = document.getElementById("passReg").value;
+    var pass2 = document.getElementById("passPReg").value;
+    if (pass1.length == 0 || pass1.length < 8) {
+        document.getElementById("greskaPassReg").className = "prikazGreske fontRoboto";
+        document.getElementById("regButton").disabled = true;
+        if (document.getElementById("greskaPassReg").className != "prikazGreske fontRoboto") {
+            brojNeispravnih++;
+        }
+    }
+    else {
+        document.getElementById("greskaPassReg").className = "hidden";
+        if (document.getElementById("greskaPassReg").className != "hidden") {
+            brojNeispravnih--;
+        }
+        if (brojNeispravnih <= 0) document.getElementById("regButton").disabled = false;
+    }
+    if (pass1.length == 0 || pass1 != pass2) {
+        document.getElementById("greskaPassPReg").className = "prikazGreske fontRoboto";
+        document.getElementById("regButton").disabled = true;
+        if (document.getElementById("greskaPassPReg").className != "prikazGreske fontRoboto") {
+            brojNeispravnih++;
+        }
+    }
+    else {
+        document.getElementById("greskaPassPReg").className = "hidden";
+        if (document.getElementById("greskaPassPReg").className != "hidden") {
+            brojNeispravnih--;
+        }
+        if (brojNeispravnih <= 0) document.getElementById("regButton").disabled = false;
+    }
+}
+
+function validirajDatum() {
+    var datRodj = document.getElementById("rodjendanReg").value;
+    if (datRodj.length == 0) {
+        document.getElementById("greskaRodjReg").className = "prikazGreske fontRoboto";
+        document.getElementById("regButton").disabled = true;
+        if (document.getElementById("greskaRodjReg").className != "prikazGreske fontRoboto") {
+            brojNeispravnih++;
+        }
+    }
+    else {
+        document.getElementById("greskaRodjReg").className = "hidden";
+        if (document.getElementById("greskaRodjReg").className != "hidden") {
+            brojNeispravnih--;
+        }
+        if (brojNeispravnih <= 0) document.getElementById("regButton").disabled = false;
+    }
+}
+
+function validirajMail(mail) {
+    var isValidMail = document.getElementById(mail).value;
+    if (!emailValidation(isValidMail) || isValidMail.length == 0) {
+        document.getElementById("greskaMailReg").className = "prikazGreske fontRoboto";
+        document.getElementById("regButton").disabled = true;
+        if (document.getElementById("greskaMailReg").className != "prikazGreske fontRoboto") {
+            brojNeispravnih++;
+        }
+    }
+    else {
+        document.getElementById("greskaMailReg").className = "hidden";
+        if (document.getElementById("greskaMailReg").className != "hidden") {
+            brojNeispravnih--;
+        }
+        if (brojNeispravnih <= 0) document.getElementById("regButton").disabled = false;
+    }
+}
+/*pomocneFunkcijeValidacija*/
+function usernameIsValid(username) {
+    var re = /^\S*$/
+    return re.test(username);
+}
+
+function emailValidation(mailForValid) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(mailForValid);
+}
+/*validacijaPostavki*/
+function validirajPostavke(validacija, greska) {
+    var ime = document.getElementById(validacija).value;
+    if (!usernameIsValid(ime) || ime.length == 0) {
+        document.getElementById(greska).className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById(greska).className = "hidden";
+    }
+}
+
+function mailPostavki(mail, greska) {
+    var isValidMail = document.getElementById(mail).value;
+    if (!emailValidation(isValidMail) || isValidMail.length == 0) {
+        document.getElementById(greska).className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById(greska).className = "hidden";
+    }
+}
+
+function datumValidacijaPostavke(datum, greska) {
+    var datRodj = document.getElementById(datum).value;
+    if (datRodj.length == 0) {
+        document.getElementById(greska).className = "prikazGreske fontRoboto";
+    }
+    else {
+        document.getElementById(greska).className = "hidden";
+    }
+}
+/*Galerija*/
+function showImage(passSource) {
+    var slika = document.getElementById("image-holder");
+    var holder = document.getElementById("ih");
+    slika.src = passSource;
+    console.log("dajem blok");
+    document.getElementById("gallery-show").style.display = 'block';
+    var sirina = holder.offsetWidth / 2 + 5;
+    holder.style.marginLeft = "-" + sirina + "px";
+}
+
+function sakrijGaleriju() {
+    document.getElementById("gallery-show").style.display = 'none';
 }
 var eskejp = function (event) {
     if (event.keyCode == 27) {
-        if (document.getElementById(idPokazanog+"_full").className == "shown") {
-            document.getElementById(idPokazanog+"_full").className = "hidden";
-        }
+        sakrijGaleriju();
     }
 }
 window.onkeydown = eskejp;
+/*dropDownMenu*/
+function dropajMeni(idMenija) {
+    document.getElementById(idMenija).classList.toggle("show");
+}
+// zatvara dropDown kada se klikne van njega
+window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+    /*AjaxLoadStranica*/
+window.onload = loadStranica('views/pocetnaView.html');
+
+function loadStranica(stranicaZaLoad) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("placeHolderSadrzajaStranice").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", stranicaZaLoad, true);
+    xhttp.send();
+}
