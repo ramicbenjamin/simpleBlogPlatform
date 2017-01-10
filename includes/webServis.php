a3bc4aa8f00a653dbcd84f429582b21b06e532f1
Benjamin Ramic <bramic2@etf.unsa.ba>
2017-01-10 20:28:42 +0100

    Commit sa svim popravkama.

--- includes/webServis.php
+++ includes/webServis.php
@@ -7,22 +7,8 @@ function zag() {
 }
 
 
-function rest_get($request, $data) { }
-function rest_post($request, $data) { }
-function rest_delete($request) { }
-function rest_put($request, $data) { }
-function rest_error($request) { }
-
-$method  = $_SERVER['REQUEST_METHOD'];
-$request = $_SERVER['REQUEST_URI'];
-
-switch($method) {
-    case 'PUT':
-        parse_str(file_get_contents('php://input'), $put_vars);
-        zag(); $data = $put_vars; rest_put($request, $data); break;
-    case 'POST':
-        zag(); $data = $_POST; rest_post($request, $data); break;
-    case 'GET':
+function rest_get($request, $data) 
+{
         $idvijesti = $_GET['id'];
         $veza = new PDO('mysql:host=' . getenv('MYSQL_SERVICE_HOST') . ';port=3306;dbname=simpleblogplatformdb', 'bramic2', 'oephaecoonee');
         $veza->exec("set names utf8");
@@ -41,7 +27,24 @@ switch($method) {
             $i++;
         }
         print "]}";
-        break;
+    
+}
+function rest_post($request, $data) { }
+function rest_delete($request) { }
+function rest_put($request, $data) { }
+function rest_error($request) { }
+
+$method  = $_SERVER['REQUEST_METHOD'];
+$request = $_SERVER['REQUEST_URI'];
+
+switch($method) {
+    case 'PUT':
+        parse_str(file_get_contents('php://input'), $put_vars);
+        zag(); $data = $put_vars; rest_put($request, $data); break;
+    case 'POST':
+        zag(); $data = $_POST; rest_post($request, $data); break;
+    case 'GET':
+        zag(); $data = $_GET; rest_get($request, $data); break;
     case 'DELETE':
         zag(); rest_delete($request); break;
     default:
